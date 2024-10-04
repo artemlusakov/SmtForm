9eb3074dc55a9d3f5488424cce9702a92aaf6454
zero <artemlysakov793@gmail.com>
2024-10-04 12:08:14 +0300

    mini save

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 // Получить модальные элементы
 let modalContacts = document.querySelector(".modalContacts");
-
 // Получить кнопки, которые открывают модальные окна
-let btn = document.getElementById("myBtn");
 let btnContacts = document.getElementById("myBtnContacts");
-
 // Получить элемент <span>, который закрывает модальные окна
 let spanContacts = document.querySelector(".closeContacts");
 
-// Когда пользователь нажимает на кнопку, открывает модальный
-btn.onclick = function() {
-  modal.style.display = "block";
-}
 
 // Анимация для модального окна контактов
 btnContacts.onclick = function() {
@@ -31,8 +24,7 @@ spanContacts.onclick = function() {
 
 // Когда пользователь щелкает в любом месте за пределами модальных окон, закрывает их
 window.onclick = function(event) {
-  if (event.target == modal || event.target == modalContacts) {
-    modal.style.display = "none";
-    modalContacts.style.display = 'none';
+  if (event.target == modalContacts) {
+    modalContacts.style.display = "none";
   }
 }
